3eac300e531d005acbda88dac70b2ebfae5b5985
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-25 19:43:17 +0200

    install npm packages

---
+++ install/installNpm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm install npm -g
+npm install gulp-cli --save-dev
+npm install babel-core babel-loader babel-preset-env --save-dev
+npm install babel-preset-es2015 --save-dev
+npm install gulp-sourcemaps vinyl-source-stream vinyl-buffer browserify watchify babelify --save-dev
+npm install gulp -D
+npm install --save-dev gulp-babel
+npm install --save-dev gulp-connect
