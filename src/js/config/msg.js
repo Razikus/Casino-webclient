b4ba73670d0a1276b5c0f3d101bcd2da36eeaa2e
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-28 12:30:30 +0000

    msg.js error fix (dynamic bundles)

--- src/js/config/msg.js
+++ src/js/config/msg.js
@@ -1,15 +1,22 @@
-import { msg } from './msg/msg-en';
 import { msgConfig } from './msgConfig';
 import { info, warn } from '../logger/logger';
 
 export function getMessageBundle(code) {
   info(`Loading msg bundle: ${code}`);
-  let bundle = require(constructBundlePath(code));
+  let bundle = getBundleFor(constructBundlePath(code));
   if(!bundle) {
     warn(`No msg bundle for code: ${code} loading default: ${msgConfig.defaultLanguage}`);
-    return require(constructBundlePath(msgConfig.defaultLanguage));
+    return getBundleFor(constructBundlePath(msgConfig.defaultLanguage));
   }
   return bundle;
+}
+
+function getBundleFor(path) {
+  try {
+    return require(path);
+  } catch(err) {
+    return undefined;
+  }
 }
 
 function constructBundlePath(code) {
