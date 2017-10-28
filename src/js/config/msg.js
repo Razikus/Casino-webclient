992fd21187e98508244ee4a659cfdb6d6c9e2cf6
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-28 20:42:50 +0200

    Workaround of runtime require

--- src/js/config/msg.js
+++ src/js/config/msg.js
@@ -3,10 +3,10 @@ import { info, warn } from '../logger/logger';
 
 export function getMessageBundle(code) {
   info(`Loading msg bundle: ${code}`);
-  let bundle = getBundleFor(constructBundlePath(code));
+  let bundle = getBundleFor(code);
   if(!bundle) {
     warn(`No msg bundle for code: ${code} loading default bundle for language: ${msgConfig.defaultLanguage}`);
-    bundle = getBundleFor(constructBundlePath(msgConfig.defaultLanguage));
+    bundle = getBundleFor(msgConfig.defaultLanguage);
     if(bundle) {
       return bundle;
     } else {
@@ -16,14 +16,13 @@ export function getMessageBundle(code) {
   return bundle;
 }
 
-function getBundleFor(path) {
-  try {
-    return require(path);
-  } catch(err) {
-    return undefined;
+function getBundleFor(code) {
+  switch(code) {
+    case "en":
+      return require("./msg/msg-en.js");
+    case "pl":
+      return require("./msg/msg-pl.js");
+    default:
+      return require(msgConfig.defaultLanguage);
   }
-}
-
-function constructBundlePath(code) {
-  return `./msg/msg-${code}`;
 }
--- src/js/config/msgConfig.js
+++ src/js/config/msgConfig.js
@@ -1,3 +1,3 @@
 export const msgConfig = {
-  defaultLanguage: 'en',
+  defaultLanguage: './msg/msg-en.js',
 }
