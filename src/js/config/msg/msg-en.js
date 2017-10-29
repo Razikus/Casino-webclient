cd12db5c9fa941965e658bcfdbaf7496c10b6ecc
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-29 08:50:55 +0100

    new translations

--- src/js/config/msg/msg-en.js
+++ src/js/config/msg/msg-en.js
@@ -2,4 +2,5 @@ export const msg = {
   connected: "Connected",
   "connected-text": "Successfully connected to casino, good luck!",
   "ERROR": "Error",
+  "EMAIL_OR_LOGIN_EXISTS" : "Email or login exists",
 }
--- src/js/config/msg/msg-pl.js
+++ src/js/config/msg/msg-pl.js
@@ -1,4 +1,6 @@
 export const msg = {
   connected: "Połączono!",
   "connected-text": "Pomyślnie połączono z kasynem!",
+  "ERROR": "Error",
+  "EMAIL_OR_LOGIN_EXISTS" : "Email lub login istnieje",
 }
